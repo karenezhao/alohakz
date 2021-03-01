43139e961a2d6f3e1f160a1091fe005473374f8e
karenezhao <karenezhao@ucsb.edu>
2021-02-28 18:15:18 -0800

    aloha commit

---
+++ R/say_aloha.R
@@ -0,0 +1,23 @@
+#' Title
+#'
+#' @param name
+#' @param print(name)
+#'
+#' @return
+#' @export
+#'
+#' @examples print("name")
+say_aloha <- function(name, print = TRUE) {
+
+  message <- paste("Aloha,",
+                   name,
+                   emo::ji("palm_tree"),
+                   emo::ji("sunny"),
+                   emo::ji("ocean"))
+
+  if (print) {
+    cat(crayon::bgGreen(message))
+  }
+
+  invisible(message)
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+
+# alohakz
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of alohakz is to ...
+
+## Installation
+
+You can install the released version of alohakz from [CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("alohakz")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(alohakz)
+## basic example code
+```
+
